d79c2c9c7f473a315c092220b3940f331cc8ed53
Piotr Pszonka <piotrallegr@gmail.com>
2017-11-23 23:57:44 +0100

    regexp

--- poker.py
+++ poker.py
@@ -16,22 +16,29 @@ import re
 import pdb
 
 PATH = 'C://repositories//files//poker_1.txt'
-FIGURES_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+FIGURES_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8',
+                 '7', '6', '5', '4', '3', '2']
 FIGURES = ''.join(FIGURES_ORDER)
 COLORS = 'HDCS'
 
-couple = lambda x: r'(([{f}])[{c}]\{p}[{c}])'.format(c='{c}', f='{f}', p=2+x)
-trio = lambda x: r'(([{f}])[{c}]\{p}[{c}]\{p}[{c}])'.format(c='{c}', f='{f}', p=2+x)
+five_cards = [FIGURES[i:i+5] for i in range(len(FIGURES)-4)]
+
+poker = r'(%s)' % '|'.join((''.join('[%s]' % j + ('([%s])' % COLORS if m == 0 else '\%d' % (n+2)) for m, j in enumerate(i))) for n, i in enumerate(five_cards))
+street = r'(%s)' % '|'.join((''.join(j + '[%s]' % COLORS for j in i)) for n, i in enumerate(five_cards))
+print('\n', poker)
+print('\n', street)
+trio = lambda x: r'(([{f}])[{c}]\%d[{c}]\%d[{c}])' % (2+x, 2+x)
+couple = lambda x: r'(([{f}])[{c}]\%d[{c}])' % (2+x)
 
 schemes = [
-    ('0_poker_krolewski', r'A[{c}]K[{c}]Q[{c}]J[{c}]T[{c}]'),
-    ('1_poker', ''),
-    ('2_kareta', r'([{f}])[{c}]\1[{c}]\1[{c}]\1[{c}]'),
-    ('3_full', r'({C0}{T1}|{T2}{C3})'.format(C0=couple(1), T1=trio(3), T2=trio(5), C3=couple(7))),
-    ('4_kolor', r'[{f}]([{c}])[{f}]\1[{f}]\1[{f}]\1[{f}]\1'),
-    ('5_street', ''),
+    ('0_poker_krolewski', r'(A([{c}])K\2Q\2J\2T\2)'),
+    ('1_poker', poker),
+    ('2_kareta', r'(([{f}])[{c}]\2[{c}]\2[{c}]\2[{c}])'),
+    ('3_full', r'(%s%s|%s%s)' % (couple(1), trio(3), trio(5), couple(7))),
+    ('4_kolor', r'([{f}]([{c}])[{f}]\2[{f}]\2[{f}]\2[{f}]\2)'),
+    ('5_street', street),
     ('6_trojka', trio(0)),
-    ('7_dwie_pary', couple(0)),
+    ('7_dwie_pary', '((?<=[{f}][{c}])%s%s|%s(?=[{f}][{c}])%s|%s%s(?=[{f}][{c}]))' % (couple(1), couple(3), couple(5), couple(7), couple(9), couple(11))),
     ('8_para', couple(0)),
     ('9_pusta', ''),
 ]
@@ -46,21 +53,17 @@ def sort_hand(hand):
 def find_pattern(hand):
     hand_aggr = [None, None, None]
     for i, exp in enumerate(schemes_exp):
-        # if i == 3: pdb.set_trace()
-        figure = re.findall(exp, hand)
+        # if i == 7: pdb.set_trace()
+        figure = re.search(exp, hand)
+        print('\n\n', i, figure)
         if figure:
-            if i in [3, 7] and len(figure) == 2:
-                hand_aggr[0] = i
-                hand_aggr[1] = (figure[0][0], figure[1][0])
-                break
-            elif i not in [3, 7] and figure[0]:
-                print('figure:', figure)
-                hand_aggr[0] = i
-                hand_aggr[1] = figure[0]
-                break
+            hand_aggr[0] = i
+            hand_aggr[1] = figure.group()
+            break
+
     hand_aggr[2] = hand
 
-    print('\n', hand_aggr)
+    print(hand_aggr)
 
     return hand_aggr
 
@@ -86,39 +89,39 @@ def test_find_pattern():
     pass
 
     # poker_krolewski:
-    assert find_pattern('AHKHQHJHTH')[0] == 0
-    assert find_pattern('AHKDQCJSTH')[0] == 0
+    # assert find_pattern('AHKHQHJHTH')[0] == 0
 
-    # poker:
-    # # assert find_pattern('') == 1
+    # # poker:
+    # assert find_pattern('KHQHJHTH9H')[0] == 1
 
     # kareta:
-    assert find_pattern('AHADACASJS')[0] == 2
-    assert find_pattern('AHJDJCJSJH')[0] == 2
+    # assert find_pattern('AHADACASJS')[0] == 2
+    # assert find_pattern('AHJDJCJSJH')[0] == 2
 
     # full:
     # assert find_pattern('AHADAS2H2D')[0] == 3
     # assert find_pattern('AHAD2S2H2D')[0] == 3
 
     # kolor:
-    assert find_pattern('AHQHJHTH9H')[0] == 4
+    # assert find_pattern('AHQHJHTH9H')[0] == 4
 
     # street:
-    # # assert find_pattern('')[0] == 5
+    # assert find_pattern('JHTD9C8S7H')[0] == 5
 
     # trojka:
-    assert find_pattern('AHADAC2SJS')[0] == 6
-    assert find_pattern('AHKDKCKHJS')[0] == 6
-    assert find_pattern('AHADQCQHQS')[0] == 6
+    # assert find_pattern('AHADAC2SJS')[0] == 6
+    # assert find_pattern('AHKDKCKHJS')[0] == 6
+    # assert find_pattern('AHKDQCQHQS')[0] == 6
 
     # dwie_pary:
-    assert find_pattern('AHADKH2H2D')[0] == 7
+    # assert find_pattern('AHKHKD3H3D')[0] == 7
     assert find_pattern('AHADKDKC2S')[0] == 7
+    assert find_pattern('AHADKSQDQC')[0] == 7
 
     # para:
-    assert find_pattern('AHKDKDQC2S')[0] == 8
-    assert find_pattern('AHKDJD3C3S')[0] == 8
-    assert find_pattern('AHKDJD3C3S')[0] == 8
+    # assert find_pattern('AHKDKDQC2S')[0] == 8
+    # assert find_pattern('AHKDJD3C3S')[0] == 8
+    # assert find_pattern('AHKDJD3C3S')[0] == 8
 
     # pusta:
     # assert find_pattern('')[0] == 9
