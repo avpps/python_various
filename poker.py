bef65b3a141e00669e030fb006e0f2d19aff5934
Piotr Pszonka <piotrallegr@gmail.com>
2017-11-24 23:16:44 +0100

    works

--- poker.py
+++ poker.py
@@ -15,7 +15,7 @@ txt file with following structure:
 import re
 import pdb
 
-PATH = 'C://repositories//files//poker_1.txt'
+PATH = 'C://repositories//files//poker.txt'
 FIGURES_ORDER = ['A', 'K', 'Q', 'J', 'T', '9', '8',
                  '7', '6', '5', '4', '3', '2']
 FIGURES = ''.join(FIGURES_ORDER)
@@ -25,8 +25,6 @@ five_cards = [FIGURES[i:i+5] for i in range(len(FIGURES)-4)]
 
 poker = r'(%s)' % '|'.join((''.join('[%s]' % j + ('([%s])' % COLORS if m == 0 else '\%d' % (n+2)) for m, j in enumerate(i))) for n, i in enumerate(five_cards))
 street = r'(%s)' % '|'.join((''.join(j + '[%s]' % COLORS for j in i)) for n, i in enumerate(five_cards))
-print('\n', poker)
-print('\n', street)
 trio = lambda x: r'(([{f}])[{c}]\%d[{c}]\%d[{c}])' % (2+x, 2+x)
 couple = lambda x: r'(([{f}])[{c}]\%d[{c}])' % (2+x)
 
@@ -38,7 +36,7 @@ schemes = [
     ('4_kolor', r'([{f}]([{c}])[{f}]\2[{f}]\2[{f}]\2[{f}]\2)'),
     ('5_street', street),
     ('6_trojka', trio(0)),
-    ('7_dwie_pary', '((?<=[{f}][{c}])%s%s|%s(?=[{f}][{c}])%s|%s%s(?=[{f}][{c}]))' % (couple(1), couple(3), couple(5), couple(7), couple(9), couple(11))),
+    ('7_dwie_pary', '((?<=[{f}][{c}])%s%s|%s[{f}][{c}]%s|%s%s(?=[{f}][{c}]))' % (couple(1), couple(3), couple(5), couple(7), couple(9), couple(11))),
     ('8_para', couple(0)),
     ('9_pusta', ''),
 ]
@@ -53,82 +51,133 @@ def sort_hand(hand):
 def find_pattern(hand):
     hand_aggr = [None, None, None]
     for i, exp in enumerate(schemes_exp):
-        # if i == 7: pdb.set_trace()
         figure = re.search(exp, hand)
-        print('\n\n', i, figure)
         if figure:
             hand_aggr[0] = i
-            hand_aggr[1] = figure.group()
+            if i == 7 and len(figure.group()) == 10:
+                hand_aggr[1] = ''.join((figure.group(6), figure.group(8)))
+            else:
+                hand_aggr[1] = figure.group()
             break
 
     hand_aggr[2] = hand
-
     print(hand_aggr)
-
     return hand_aggr
 
+remisy = [0]
 
-def compare(player_1, player_2, result):
-    result_updated = None
+def compare(hand_1, hand_2, result):
+    if hand_1[0] < hand_2[0]:
+        result['gracz_1'] += 1
+        return result
+    elif hand_1[0] > hand_2[0]:
+        result['gracz_2'] += 1
+        return result
+    elif hand_1[0] == hand_2[0]:
+        if hand_1[0] == 9:
+            for i, j in zip(hand_1[2][::2], hand_2[2][::2]):
+                if FIGURES_ORDER.index(i) < FIGURES_ORDER.index((j)):
+                    result['gracz_1'] += 1
+                    return result
+                elif FIGURES_ORDER.index(i) > FIGURES_ORDER.index((j)):
+                    result['gracz_2'] += 1
+                    return result
+            else:
+                return result
+        else:
+            if FIGURES_ORDER.index(hand_1[1][0]) < FIGURES_ORDER.index(hand_2[1][0]):
+                result['gracz_1'] += 1
+                return result
+            elif FIGURES_ORDER.index(hand_1[1][0]) > FIGURES_ORDER.index(hand_2[1][0]):
+                result['gracz_2'] += 1
+                return result
+            else:
+                for i, j in zip(hand_1[2][::2], hand_2[2][::2]):
+                    if FIGURES_ORDER.index(i) < FIGURES_ORDER.index((j)):
+                        result['gracz_1'] += 1
+                        return result
+                    elif FIGURES_ORDER.index(i) > FIGURES_ORDER.index((j)):
+                        result['gracz_2'] += 1
+                        return result
+                else:
+                    remisy[0] += 1
+                    return result
+
+    result_updated = result
     return result_updated
 
 
-# result = {'gracz_1': 0, 'gracz_2': 0}
-#
-# with open(PATH) as file:
-#     for line in file:
-#         hands = line.replace('\n', '').split(' ')
-#         hand_1 = find_pattern(sort_hand(hands[:5]))
-#         hand_2 = find_pattern(sort_hand(hands[5:]))
-#         result = compare(hand_1, hand_2, result)
-#
-# print(result['gracz_1'])
+result = {'gracz_1': 0, 'gracz_2': 0}
+
+with open(PATH) as file:
+    for line in file:
+        hands = line.replace('\n', '').split(' ')
+        hand_1 = find_pattern(sort_hand(hands[:5]))
+        hand_2 = find_pattern(sort_hand(hands[5:]))
+        result = compare(hand_1, hand_2, result)
+
+print('\n\n', result, remisy)
 
 
 def test_find_pattern():
     pass
 
     # poker_krolewski:
-    # assert find_pattern('AHKHQHJHTH')[0] == 0
+    assert find_pattern('AHKHQHJHTH')[0] == 0
 
     # # poker:
-    # assert find_pattern('KHQHJHTH9H')[0] == 1
+    assert find_pattern('KHQHJHTH9H')[0] == 1
 
     # kareta:
-    # assert find_pattern('AHADACASJS')[0] == 2
-    # assert find_pattern('AHJDJCJSJH')[0] == 2
+    assert find_pattern('AHADACASJS')[0] == 2
+    assert find_pattern('AHJDJCJSJH')[0] == 2
 
     # full:
-    # assert find_pattern('AHADAS2H2D')[0] == 3
-    # assert find_pattern('AHAD2S2H2D')[0] == 3
+    assert find_pattern('AHADAS2H2D')[0] == 3
+    assert find_pattern('AHAD2S2H2D')[0] == 3
 
     # kolor:
-    # assert find_pattern('AHQHJHTH9H')[0] == 4
+    assert find_pattern('AHQHJHTH9H')[0] == 4
 
     # street:
-    # assert find_pattern('JHTD9C8S7H')[0] == 5
+    assert find_pattern('JHTD9C8S7H')[0] == 5
 
     # trojka:
-    # assert find_pattern('AHADAC2SJS')[0] == 6
-    # assert find_pattern('AHKDKCKHJS')[0] == 6
-    # assert find_pattern('AHKDQCQHQS')[0] == 6
+    assert find_pattern('AHADAC2SJS')[0] == 6
+    assert find_pattern('AHKDKCKHJS')[0] == 6
+    assert find_pattern('AHKDQCQHQS')[0] == 6
 
     # dwie_pary:
-    # assert find_pattern('AHKHKD3H3D')[0] == 7
+    assert find_pattern('AHKHKD3H3D')[0] == 7
     assert find_pattern('AHADKDKC2S')[0] == 7
     assert find_pattern('AHADKSQDQC')[0] == 7
 
     # para:
-    # assert find_pattern('AHKDKDQC2S')[0] == 8
-    # assert find_pattern('AHKDJD3C3S')[0] == 8
-    # assert find_pattern('AHKDJD3C3S')[0] == 8
+    assert find_pattern('AHKDKDQC2S')[0] == 8
+    assert find_pattern('AHKDJD3C3S')[0] == 8
+    assert find_pattern('AHKDJD3C3S')[0] == 8
 
     # pusta:
-    # assert find_pattern('')[0] == 9
+    assert find_pattern('ASKHQHJH2H')[0] == 9
 
 
+def test_compare():
+    result = {'gracz_1': 0, 'gracz_2': 0}
 
+    resp = compare([3, 'AHADAS2H2D', 'AHADAS2H2D'], [4, 'AHQHJHTH9H', 'AHQHJHTH9H'], result)
+    assert resp == {'gracz_1': 1, 'gracz_2': 0}
 
+    result = {'gracz_1': 0, 'gracz_2': 0}
 
+    resp = compare([9, '', 'ASKDJD8H3D'], [9, '', 'QD8C7C6C5C'], result)
+    assert resp == {'gracz_1': 1, 'gracz_2': 0}
 
+    result = {'gracz_1': 0, 'gracz_2': 0}
 
+    resp = compare([3, 'AHADAS2H2D', 'AHADAS2H2D'], [3, 'KHKD2S2H2D', 'KHKD2S2H2D'], result)
+    assert resp == {'gracz_1': 1, 'gracz_2': 0}
+
+    result = {'gracz_1': 0, 'gracz_2': 0}
+
+    resp = compare([8, 'AHKDJS2H2D', 'KHQD5S2H2D'], [8, 'KHKD2S2H2D', 'KHKD2S2H2D'], result)
+    assert resp == {'gracz_1': 1, 'gracz_2': 0}
